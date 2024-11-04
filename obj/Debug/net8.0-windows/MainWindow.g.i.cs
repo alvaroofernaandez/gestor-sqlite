979ff9be8c5716f774c19bb96a7b09f729d715d1
Álvaro Fernández Amaro <alvaroofernaandez@gmail.com>
2024-11-04 16:27:37 +0100

    arreglo en rutas bbdd

--- obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -125,7 +125,7 @@ namespace UA3Tarea1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/UA3Tarea1;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/UA3Tarea1;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- obj/Debug/net8.0-windows/UA3Tarea1.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/UA3Tarea1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UA3Tarea1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a922bf33c01552f2e6acba079ddeed5ac39b123")]
 [assembly: System.Reflection.AssemblyProductAttribute("UA3Tarea1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UA3Tarea1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
